512953b63531851598a0a8d72acd23962a98c4af
Roman <masorinwork@gmail.com>
2019-07-09 18:22:08 +0300

    init composer

---
+++ src/Test/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Application\Test;
+
+/**
+ * Test class
+ *
+ * @author Roman
+ */
+class Test
+{
+
+    /**
+     * @return string test string
+     */
+    static function init()
+    {
+        return __METHOD__;
+    }
+
+}
